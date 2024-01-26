deab933eb333c56e40254b4827b919250af769c4
Paulina San Martin <puli.sanmartin@gmail.com>
2024-01-26 16:25:53 -0400

    Adición scroll y alert en boton 'Enviar'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-document.getElementById('#enviar').onclick = function(){
-    alert('¡Gracias por su interés! Responderemos su mensaje a la brevedad');
+document.getElementById('enviar').onclick = function(){
+    alert('¡Listo! Nos contactaremos con usted a la brevedad');
 }
