eeeb1a7fcec16d77d5dc6116e74b6c8188a95ab2
Paulina San Martin <puli.sanmartin@gmail.com>
2024-01-26 15:46:08 -0400

    Cambio familia tipográfica a Raleway

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.getElementById('#enviar').onclick = function(){
+    alert('¡Gracias por su interés! Responderemos su mensaje a la brevedad');
+}
