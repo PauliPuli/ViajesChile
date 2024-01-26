18a97f14d4ff8cfa30cfc66e9572ddd11fecd5c0
Paulina San Martin <puli.sanmartin@gmail.com>
2024-01-26 17:33:23 -0400

    Navbar transparente y con fondo oscura al navegar. Inclusión de acción en jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 document.getElementById('enviar').onclick = function(){
     alert('¡Listo! Nos contactaremos con usted a la brevedad');
 }
+
+// tutorial https://youtu.be/kmmxxfyufLE?si=Xb0LBuLY-4mEz0Gt&t=598 cambié 'var' por 'let' y 'addEventListener' por 'scrollY'//
+let nav = document.querySelector('nav');
+window.addEventListener('scroll', function(){
+    if(this.window.scrollY > 1000){
+        nav.classList.add('bg-dark');
+    }else{
+        nav.classList.remove('bg-dark');
+    }
+})
